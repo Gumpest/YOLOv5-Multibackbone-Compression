1db329bcb8a275f113d1cddc466f0775505332cf
Gumpest <gump_well_done@163.com>
2021-11-02 22:08:53 +0800

    add Shffulenet backbone

--- models/common.py
+++ models/common.py
@@ -32,6 +32,72 @@ def autopad(k, p=None):  # kernel, padding
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
 
+#-------------------------------------------------------------------------
+# ShuffleNetV2
+class Conv_maxpool(nn.Module):  
+    def __init__(self, c1, c2):  # ch_in, ch_out  
+        super().__init__()  
+        self.conv= nn.Sequential(
+            nn.Conv2d(c1, c2, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(c2),
+            nn.ReLU(inplace=True),
+        )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+
+    def forward(self, x):  
+        return self.maxpool(self.conv(x))
+
+class ShuffleNetV2_InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride):  # ch_in, ch_out, stride
+        super().__init__()
+
+        self.stride = stride
+
+        branch_features = oup // 2
+        assert (self.stride != 1) or (inp == branch_features << 1)
+
+        if self.stride == 2:
+            # copy input
+            self.branch1 = nn.Sequential(
+                nn.Conv2d(inp, inp, kernel_size=3, stride=self.stride, padding=1, groups=inp),
+                nn.BatchNorm2d(inp),
+                nn.Conv2d(inp, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(branch_features),
+                nn.ReLU(inplace=True))
+        else:
+            self.branch1 = nn.Sequential()
+
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(inp if (self.stride == 2) else branch_features, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(branch_features, branch_features, kernel_size=3, stride=self.stride, padding=1, groups=branch_features),
+            nn.BatchNorm2d(branch_features),
+
+            nn.Conv2d(branch_features, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        if self.stride == 1:
+            x1, x2 = x.chunk(2, dim=1)
+            out = torch.cat((x1, self.branch2(x2)), dim=1)
+        else:
+            out = torch.cat((self.branch1(x), self.branch2(x)), dim=1)
+
+        out = self.channel_shuffle(out, 2)
+
+        return out
+
+    def channel_shuffle(self, x, groups):
+        N, C, H, W = x.size()
+        out = x.view(N, groups, C // groups, H, W).permute(0, 2, 1, 3, 4).contiguous().view(N, C, H, W)
+
+        return out
+# -------------------------------------------------------------------------
+# CBAM
 class ChannelAttention(nn.Module):
     def __init__(self, in_planes, ratio=16):
         super(ChannelAttention, self).__init__()
@@ -74,7 +140,8 @@ class CBAM(nn.Module):
         out = self.spatial_attention(out) * out
         return out
 
-# Adaptive avg pooling->FC->FC->Sigmoid
+# -------------------------------------------------------------------------
+# SE-Net Adaptive avg pooling --> fc --> fc --> Sigmoid
 class SeBlock(nn.Module):
     def __init__(self, in_channel, reduction=4):
         super().__init__()
