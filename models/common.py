e7d0abb4f965ceb91a75ab19199917946f1b919c
Gumpest <gump_well_done@163.com>
2021-11-05 22:37:09 +0800

    add mobilev3small

--- models/common.py
+++ models/common.py
@@ -33,6 +33,69 @@ def autopad(k, p=None):  # kernel, padding
     return p
 
 #-------------------------------------------------------------------------
+# Mobilenetv3Small
+class conv_bn_hswish(nn.Module):
+    """
+    This equals to
+    def conv_3x3_bn(inp, oup, stride):
+        return nn.Sequential(
+            nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+            nn.BatchNorm2d(oup),
+            h_swish()
+        )
+    """
+    def __init__(self, c1, c2, stride):
+        super(conv_bn_hswish, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, 3, stride, 1, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.Hardswish()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+class MobileNetV3_InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, hidden_dim, kernel_size, stride, use_se, use_hs):
+        super(MobileNetV3_InvertedResidual, self).__init__()
+        assert stride in [1, 2]
+
+        self.identity = stride == 1 and inp == oup
+
+        if inp == hidden_dim:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.Hardswish() if use_hs else nn.ReLU(),
+                # Squeeze-and-Excite
+                SeBlock(hidden_dim) if use_se else nn.Sequential(),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            self.conv = nn.Sequential(
+                # pw
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.Hardswish() if use_hs else nn.ReLU(),
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                # Squeeze-and-Excite
+                SeBlock(hidden_dim) if use_se else nn.Sequential(),
+                nn.Hardswish() if use_hs else nn.ReLU(),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
+    def forward(self, x):
+        y = self.conv(x)
+        if self.identity:
+            return x + y
+        else:
+            return y
+#-------------------------------------------------------------------------
 # ShuffleNetV2
 class Conv_maxpool(nn.Module):  
     def __init__(self, c1, c2):  # ch_in, ch_out  
@@ -155,8 +218,8 @@ class SeBlock(nn.Module):
 
     def forward(self, x):
         y = self.Squeeze(x)
-        y = self.Excitation(y)
-        return x.mul_(y.expand_as(x))
+        ouput = self.Excitation(y)
+        return x*(ouput.expand_as(x))
 
 
 class Conv(nn.Module):
