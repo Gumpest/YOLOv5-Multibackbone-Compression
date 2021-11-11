09fda9e194f109a5cf014bf90bf2a024a5f1464f
Gumpest <gump_well_done@163.com>
2021-11-11 22:43:29 +0800

    fix yolov5x-tph

--- utils/loss.py
+++ utils/loss.py
@@ -121,6 +121,7 @@ class ComputeLoss:
 
         # Losses
         for i, pi in enumerate(p):  # layer index, layer predictions
+            # [8, 3, 76/38/19, 76/38/19, 15]
             b, a, gj, gi = indices[i]  # image, anchor, gridy, gridx
             tobj = torch.zeros_like(pi[..., 0], device=device)  # target obj
 
@@ -159,6 +160,8 @@ class ComputeLoss:
 
         if self.autobalance:
             self.balance = [x / self.balance[self.ssi] for x in self.balance]
+
+        # 根据hyp.scratch.yaml超参数 对各个损失函数加权
         lbox *= self.hyp['box']
         lobj *= self.hyp['obj']
         lcls *= self.hyp['cls']
@@ -166,41 +169,66 @@ class ComputeLoss:
 
         return (lbox + lobj + lcls) * bs, torch.cat((lbox, lobj, lcls)).detach()
 
+    # Build targets for compute_loss(), input targets(image, class, x,y,w,h)
     def build_targets(self, p, targets):
-        # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
+        # 预测 list中有3组 每组的shape [8, 3, 76/38/19, 76/38/19, 15] [b, anchor, h, w, (10+4+1)]
+        # 标签 386组 [386, 6] [0.00000, 3.00000, 0.36691, 0.13881, 0.03716, 0.06403] [image, class, x, y, w, h]
+        
         na, nt = self.na, targets.shape[0]  # number of anchors, targets
+        # print(na, nt) 3 386
+
         tcls, tbox, indices, anch = [], [], [], []
         gain = torch.ones(7, device=targets.device)  # normalized to gridspace gain
         ai = torch.arange(na, device=targets.device).float().view(na, 1).repeat(1, nt)  # same as .repeat_interleave(nt)
-        targets = torch.cat((targets.repeat(na, 1, 1), ai[:, :, None]), 2)  # append anchor indices
+        # [0.,1.,2.] --> [[0.], [1.], [2]] --> 扩展nt列 torch.Size([3, 386])
 
-        g = 0.5  # bias
-        off = torch.tensor([[0, 0],
-                            [1, 0], [0, 1], [-1, 0], [0, -1],  # j,k,l,m
-                            # [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
+        # target复制3份，同时多一维anchor序号
+        targets = torch.cat((targets.repeat(na, 1, 1), ai[:, :, None]), 2)  # append anchor indices
+        # [386, 6] --> [3, 386, 6] + [3, 386, 1] --> [3, 386, 7])  [anchor, target, (image, class, x, y, w, h, anchor_ind)]
+        g = 0.5  # bias 网格中心
+        # 自己 + 上下左右4个网格
+        off = torch.tensor([[0, 0], [1, 0], [0, 1], [-1, 0], [0, -1],  # j,k,l,m
                             ], device=targets.device).float() * g  # offsets
 
+        # print(self.nl) 3
         for i in range(self.nl):
+            # print(self.anchors[i])
+            
+            # tensor([[0.34009, 0.52490],
+            #         [0.49854, 1.03906],
+            #         [0.90088, 0.74414]], device='cuda:0')
+            # tensor([[0.43945, 0.77930],
+            #         [0.92285, 0.55029],
+            #         [0.76367, 1.09863]], device='cuda:0')
+            # tensor([[0.85840, 0.45386],
+            #         [0.70508, 0.90479],
+            #         [1.44629, 1.11914]], device='cuda:0')
+
             anchors = self.anchors[i]
-            gain[2:6] = torch.tensor(p[i].shape)[[3, 2, 3, 2]]  # xyxy gain
+            gain[2:6] = torch.tensor(p[i].shape)[[3, 2, 3, 2]]  # xyxy gain [1 1 76 76 76 76 1]
 
             # Match targets to anchors
-            t = targets * gain
+            # 将ground truth坐标（x,y,h,w）转换到对应层的特征图上
+            t = targets * gain  # [3, 386, 7]
+
             if nt:
                 # Matches
                 r = t[:, :, 4:6] / anchors[:, None]  # wh ratio
-                j = torch.max(r, 1. / r).max(2)[0] < self.hyp['anchor_t']  # compare
-                # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))
-                t = t[j]  # filter
+                # print(r.shape) torch.Size([3, 386, 2])
+                j = torch.max(r, 1. / r).max(2)[0] < self.hyp['anchor_t']  # 386组target与本层3个anchor相似度 最大值是否小于4的索引 [3, 386]
+                t = t[j]  # filter [3*386, 7] --> [616, 7]
 
                 # Offsets
-                gxy = t[:, 2:4]  # grid xy
-                gxi = gain[[2, 3]] - gxy  # inverse
-                j, k = ((gxy % 1. < g) & (gxy > 1.)).T
+                gxy = t[:, 2:4]             # grid xy [25.517, 2.141] 左上为原点的坐标值
+                gxi = gain[[2, 3]] - gxy    # inverse xi [] 转换为右下为原点的坐标值
+                # 选择最近的两个邻居网格 j, k, l, m代表四个邻居
+                j, k = ((gxy % 1. < g) & (gxy > 1.)).T  # False, True
                 l, m = ((gxi % 1. < g) & (gxi > 1.)).T
-                j = torch.stack((torch.ones_like(j), j, k, l, m))
-                t = t.repeat((5, 1, 1))[j]
-                offsets = (torch.zeros_like(gxy)[None] + off[:, None])[j]
+
+                j = torch.stack((torch.ones_like(j), j, k, l, m)) # 按行stack [5, 616]
+ 
+                t = t.repeat((5, 1, 1))[j]  # [616, 7] --> [5, 616, 7] 为十字五个坐标复制5份 --> [5*616, 7] --> [1821, 7]
+                offsets = (torch.zeros_like(gxy)[None] + off[:, None])[j] # 5个坐标的offset [1821, 2]
             else:
                 t = targets[0]
                 offsets = 0
@@ -209,12 +237,12 @@ class ComputeLoss:
             b, c = t[:, :2].long().T  # image, class
             gxy = t[:, 2:4]  # grid xy
             gwh = t[:, 4:6]  # grid wh
-            gij = (gxy - offsets).long()
+            gij = (gxy - offsets).long() # 对应5个坐标减去对应offset
             gi, gj = gij.T  # grid xy indices
 
             # Append
             a = t[:, 6].long()  # anchor indices
-            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
+            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid xy
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
